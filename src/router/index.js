a104e65800afa20f669920446a9891a661d7a136
unknown <s.p.antonyuk@gmail.com>
2020-02-07 20:33:34 +0200

    fix: after lesson 2

--- src/router/index.js
+++ src/router/index.js
@@ -9,11 +9,6 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
-      name: 'Home',
-      component: Home
-    },
-    {
       path: '/login',
       name: 'login',
       meta: { layout: 'empty' },
@@ -26,14 +21,20 @@ const router = new VueRouter({
       component: () => import('../views/Register.vue')
     },
     {
+      path: '/',
+      name: 'home',
+      meta: { layout: 'main' },
+      component: () => import('../views/Home.vue')
+    },
+    {
       path: '/categories',
       name: 'categories',
       meta: { layout: 'main' },
       component: () => import('../views/Categories.vue')
     },
     {
-      path: '/detail-record',
-      name: 'detail-record',
+      path: '/detail/:id',
+      name: 'detail',
       meta: { layout: 'main' },
       component: () => import('../views/DetailRecord.vue')
     },
