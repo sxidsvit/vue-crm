6dddb5201af2807b6f6fbaba5832aa67d03cc839
unknown <s.p.antonyuk@gmail.com>
2020-02-08 21:35:26 +0200

    Lesonn 5 email validation done

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,14 +1,19 @@
 <template>
-  <form class="card auth-card">
+  <form class="card auth-card" @submit.prevent="submitHandler">
     <div class="card-content">
       <span class="card-title">Домашняя бухгалтерия</span>
       <div class="input-field">
-        <input id="email" type="text" class="validate" />
+        <input
+          id="email"
+          type="text"
+          v-model.trim="email"
+          :class="{invalid: ($v.email.$dirty && !$v.email.required)||($v.email.$dirty && !$v.email.email)}"
+        />
         <label for="email">Email</label>
         <small class="helper-text invalid">Email</small>
       </div>
       <div class="input-field">
-        <input id="password" type="password" class="validate" />
+        <input id="password" type="password" v-model="password" />
         <label for="password">Пароль</label>
         <small class="helper-text invalid">Password</small>
       </div>
@@ -23,8 +28,33 @@
 
       <p class="center">
         Нет аккаунта?
-        <a href="/">Зарегистрироваться</a>
+        <router-link to="/register">Зарегистрироваться</router-link>
       </p>
     </div>
   </form>
 </template>
+
+<script>
+import { email, required, minLength } from "vuelidate/lib/validators";
+export default {
+  name: "login",
+  data: () => ({
+    email: "",
+    password: ""
+  }),
+  validations: {
+    email: { email, required },
+    password: { required, minLenght: minLength(6) }
+  },
+  methods: {
+    submitHandler() {
+      // здесь предполагается: валидация + запрос к firebase
+      // if (this.$v.$inavalid) {
+      //   this.$v.$touch();
+      //   return;
+      // }
+      this.$router.push("/");
+    }
+  }
+};
+</script> 
