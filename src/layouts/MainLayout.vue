5f78dc6ea41e6b1291d4f2c00675242336765bf5
unknown <s.p.antonyuk@gmail.com>
2020-02-08 14:28:49 +0200

    After lesson 4

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -11,9 +11,9 @@
     </main>
 
     <div class="fixed-action-btn">
-      <a class="btn-floating btn-large blue" href="#">
+      <router-link to="/record" class="btn-floating btn-large blue">
         <i class="large material-icons">add</i>
-      </a>
+      </router-link>
     </div>
   </div>
 </template>
