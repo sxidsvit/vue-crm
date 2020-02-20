834951844cdd5c09c17a577cd75b70f75d0476a1
unknown <s.p.antonyuk@gmail.com>
2020-02-20 21:17:03 +0200

    localizi.filter.js  ru-RU/en-US

--- src/filters/localize.filter.js
+++ src/filters/localize.filter.js
@@ -8,6 +8,6 @@ const locales = {
 }
 
 export default function localizeFilter(key) {
-  const locale = store.getters.info.locale || ''
+  const locale = store.getters.info.locale || 'ru-RU'
   return locales[locale][key] || `[Localize error]: key ${key} not found`
 }
